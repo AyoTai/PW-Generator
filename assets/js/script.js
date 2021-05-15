2b9aa168feeedfb93e45ddc5f66b9bc396b1ae0c
Tai Mujarin <tai.mujarin@gmail.com>
2021-05-15 16:36:20 -0700

    added a prompt to confirm length and char

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,41 @@
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upper = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lower = "qwertyuiopasdfghjklzxcvbnm";
+var number = "1234567890";
+var symbol = "!@#$%^&*(){}[]=<>/,.";
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+var pLength = prompt("How many Characters would you like your password to be?");
+
+while (pLength < 8 || pLength > 128) {
+pLength = prompt("Length Must be 8 to 128 Characters. How many Characters would you like your password to be?");
 }
 
-generateBtn.addEventListener("click", writePassword);
+var conUpper = confirm("Do you want to use uppercase letters?");
+var conLower = confirm("Do you want to use to use lowercase letters?");
+var conNum = confirm("Do you want to use to use numbers?");
+var conSymb = confirm("Do you want to use to use special characters?");
+
+while (!(conUpper || conLower || conNum || conSymb)) {
+  alert("You must select at least one character type!");
+  conUpper = confirm("Do you want to use uppercase letters?");
+  conLower = confirm("Do you want to use lowercase letters?");
+  conNum = confirm("Do you want to use numbers?");
+  conSymb = confirm("Do you want to use special characters?");
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword(upper, lower, number, symbol, pLength);
+  var passwordText = document.querySelector("#password");
+  
+  for (var i = 0, n = generatePassword.length; i < length; ++i) {
+    passwordText += password.charAt(Math.floor(Math.random() * n));
+}
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
